a5bd120c24c81815d4ab1e826b2f8b5aae0de44f
Alan Meinberg <alantp2013@gmail.com>
2026-02-06 07:44:33 -0300

    Cadastro do Dyogo cadastro de batidas

--- sgc/src/Modelos/CadastroBatidas.jsx
+++ sgc/src/Modelos/CadastroBatidas.jsx
@@ -12,7 +12,7 @@ import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
 
-function CadastroBatidas () {
+function CadastroBatidas() {
 
     const hoje = new Date();
     hoje.setHours(0, 0, 0, 0);
@@ -29,7 +29,8 @@ function CadastroBatidas () {
     const opcoes = {
         a: 'Aquiles',
         b: 'Rodrigo',
-        c: 'Tunico'
+        c: 'Tunico',
+        d: 'Dyogo'
     };
 
 
@@ -88,11 +89,11 @@ function CadastroBatidas () {
         setData(new Date());
     };
 
-    return(
+    return (
         <div>
             <Navbar className='justify-content-center'>
                 <Navbar.Brand>
-                    <img src={logo} alt='logotipo fenix' height={100} width={100} />
+                    <img src={logo} alt='logotipo fenix' height={100} width={100}/>
                 </Navbar.Brand>
                 <Navbar.Brand>
                     <b className={styles.titulo}>Fênix Soluções em Embalagens</b>
@@ -102,112 +103,115 @@ function CadastroBatidas () {
             <div className={styles.subtitulo}>
                 <h3>Cadastro de Batidas</h3>
             </div>
-          <div>
-              <Form onSubmit={handleSubmit}>
-                  <div>
-                      <Row className={styles.rowForm}>
-                          <Col md={3}></Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Colaborador</label>
-                              <select
-                                  required
-                                  className={``}
-                                  value={colaborador}
-                                  onChange={(e) => setColaborador(e.target.value)}
-                              >
-                                  <option value="">Selecione...</option>
-                                  {Object.entries(opcoes).map(([valor, texto]) => (
-                                      <option key={valor} value={texto}>
-                                          {texto}
-                                      </option>
-                                  ))}
-                              </select>
-                          </Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Batidas Normais</label>
-                              <input
-                                  required
-                                  type={"text"}
-                                  className= {`form-control ${styles.formInput}`}
-                                  value={batida_normal}
-                                  onChange={(e) => setBatidasNormais(e.target.value)}
-                              />
-                          </Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Batidas Extras</label>
-                              <input
-                                  required
-                                  type={"text"}
-                                  value={batida_extra}
-                                  className= {`form-control ${styles.formInput}`}
-                                  onChange={(e) => setBatidasExtras(e.target.value)}
-                              />
-                          </Col>
-                          <Col md={3}></Col>
+            <div>
+                <Form onSubmit={handleSubmit}>
+                    <div>
+                        <Row className={styles.rowForm}>
+                            <Col md={3}></Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Colaborador</label>
+                                <select
+                                    required
+                                    className={``}
+                                    value={colaborador}
+                                    onChange={(e) => setColaborador(e.target.value)}
+                                >
+                                    <option value="">Selecione...</option>
+                                    {Object.entries(opcoes).map(([valor, texto]) => (
+                                        <option key={valor} value={texto}>
+                                            {texto}
+                                        </option>
+                                    ))}
+                                </select>
+                            </Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Batidas Normais</label>
+                                <input
+                                    required
+                                    type={"text"}
+                                    className={`form-control ${styles.formInput}`}
+                                    value={batida_normal}
+                                    onChange={(e) => setBatidasNormais(e.target.value)}
+                                />
+                            </Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Batidas Extras</label>
+                                <input
+                                    required
+                                    type={"text"}
+                                    value={batida_extra}
+                                    className={`form-control ${styles.formInput}`}
+                                    onChange={(e) => setBatidasExtras(e.target.value)}
+                                />
+                            </Col>
+                            <Col md={3}></Col>
 
-                      </Row>
-                      <Row className={styles.rowForm}>
-                          <Col md={3}></Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>amostra</label>
-                              <input
-                                  required
-                                  type={"text"}
-                                  value={amostra}
-                                  className= {`form-control ${styles.formInput}`}
-                                  onChange={(e) => setamostra(e.target.value)}
-                              />
-                          </Col>
+                        </Row>
+                        <Row className={styles.rowForm}>
+                            <Col md={3}></Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>amostra</label>
+                                <input
+                                    required
+                                    type={"text"}
+                                    value={amostra}
+                                    className={`form-control ${styles.formInput}`}
+                                    onChange={(e) => setamostra(e.target.value)}
+                                />
+                            </Col>
 
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Percas</label>
-                              <input
-                                  required
-                                  type={"text"}
-                                  value={perdas}
-                                  className= {`form-control ${styles.formInput}`}
-                                  onChange={(e) => setPercas(e.target.value)}
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Percas</label>
+                                <input
+                                    required
+                                    type={"text"}
+                                    value={perdas}
+                                    className={`form-control ${styles.formInput}`}
+                                    onChange={(e) => setPercas(e.target.value)}
 
-                              />
-                          </Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Meta</label>
-                              <input
-                                  type={"text"}
-                                  value={meta}
-                                  className= {`form-control ${styles.formInput}`}
-                                  onChange={(e) => setMeta(e.target.value)}
+                                />
+                            </Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Meta</label>
+                                <input
+                                    type={"text"}
+                                    value={meta}
+                                    className={`form-control ${styles.formInput}`}
+                                    onChange={(e) => setMeta(e.target.value)}
 
-                              />
-                          </Col>
-                          <Col md={3}></Col>
-                      </Row>
-                      <Row className={styles.rowForm}>
-                          <Col md={3}></Col>
-                          <Col md={2}></Col>
-                          <Col md={2} className={styles.colForm}>
-                              <label className={styles.labelText}>Data</label>
-                              <div>
-                                  <DatePicker showMonthYearDropdown={""}
-                                              selected={data}
-                                              className={`form-control ${styles.formInput}`}
-                                              onChange={(data) => setData(data)}
-                                              dateFormat="dd/MM/yyyy"
-                                  />
-                              </div>
-                          </Col>
-                      </Row>
-                      <Row className={styles.rowForm}>
-                          <Container className={styles.containerBotoes}>
-                              <Button variant="outline-primary" className={styles.buttonForm} onClick={handleClear}> Limpar </Button>
-                              <Button variant="outline-primary" type="submit" className={styles.buttonForm}> Salvar </Button>
-                              <Button variant="outline-primary"  className={styles.buttonForm} as={Link} to='/Home'> Voltar </Button>
-                          </Container>
+                                />
+                            </Col>
+                            <Col md={3}></Col>
+                        </Row>
+                        <Row className={styles.rowForm}>
+                            <Col md={3}></Col>
+                            <Col md={2}></Col>
+                            <Col md={2} className={styles.colForm}>
+                                <label className={styles.labelText}>Data</label>
+                                <div>
+                                    <DatePicker showMonthYearDropdown={""}
+                                                selected={data}
+                                                className={`form-control ${styles.formInput}`}
+                                                onChange={(data) => setData(data)}
+                                                dateFormat="dd/MM/yyyy"
+                                    />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className={styles.rowForm}>
+                            <Container className={styles.containerBotoes}>
+                                <Button variant="outline-primary" className={styles.buttonForm}
+                                        onClick={handleClear}> Limpar </Button>
+                                <Button variant="outline-primary" type="submit"
+                                        className={styles.buttonForm}> Salvar </Button>
+                                <Button variant="outline-primary" className={styles.buttonForm} as={Link}
+                                        to='/Home'> Voltar </Button>
+                            </Container>
 
-                      </Row>
-                  </div>
-              </Form>
-          </div>
+                        </Row>
+                    </div>
+                </Form>
+            </div>
         </div>
     )
 }
