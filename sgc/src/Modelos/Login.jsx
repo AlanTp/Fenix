393a6f1511b3fb5da125bbc2fa1f05db0a937962
Alan Meinberg <alantp2013@gmail.com>
2026-03-19 11:32:50 -0300

    teste api

--- sgc/src/Modelos/Login.jsx
+++ sgc/src/Modelos/Login.jsx
@@ -2,59 +2,59 @@ import {Navbar} from "react-bootstrap";
 import logo from "../imagens/logo.png";
 import Container from "react-bootstrap/Container";
 import styles from "../Estilos/Login.module.css";
-import React from "react";
+import React, {useState} from "react";
 import Col from 'react-bootstrap/Col';
 import Row from "react-bootstrap/Row";
-import {useState} from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {Spinner} from "reactstrap";
 
 function Login() {
-        const [login, setLogin] = useState("");
-        const [senha, setSenha] = useState("");
-        const [erro, setErro] = useState("");
-        const navigate = useNavigate();
-        const [loading, setLoading] = useState(false);
+    const [login, setLogin] = useState("");
+    const [senha, setSenha] = useState("");
+    const [erro, setErro] = useState("");
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
-        const handleLogin = async (e) => {
-            e.preventDefault();
-            setErro("");
+    const handleLogin = async (e) => {
+        console.log("🔥 HANDLE LOGIN DISPAROU");
+        e.preventDefault();
+        setErro("");
 
-            try {
-                setLoading(true);
+        try {
+            setLoading(true);
+            console.log("RODANDO NO:", typeof window);
+            const response = await fetch("https://fenix-api-gkyb.onrender.com/Login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({login, senha}),
+            });
 
-                const response = await fetch("http://localhost:4000/Login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({login, senha}),
-                });
+            const data = await response.json();
 
-                const data = await response.json();
-
-                if (!response.ok) {
-                    setErro(data.erro || "Falha ao fazer login");
-                    return;
-                }
-
-                // Armazena token e nome
-                localStorage.setItem("token", data.token);
-                localStorage.setItem("usuario", data.nome);
-
-                // Redireciona pra home ou dashboard
-                navigate("/home");
-            } catch (error) {
-                console.error("Erro na requisição:", error);
-                setErro("Erro de conexão com o servidor");
-            }finally {
-                setLoading(false);
+            if (!response.ok) {
+                setErro(data.erro || "Falha ao fazer login");
+                return;
             }
-        }
 
-    return(
-        <div >
-            <Navbar md={3} className='justify-content-center' >
+            // Armazena token e nome
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("usuario", data.nome);
+
+            // Redireciona pra home ou dashboard
+            navigate("/home");
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            setErro("Erro de conexão com o servidor");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div>
+            <Navbar md={3} className='justify-content-center'>
                 <Navbar.Brand><img src={logo} alt='logotipo fenix' height={100} width={100}/></Navbar.Brand>
                 <Navbar.Brand><b className={styles.titulo}>Fênix Soluções em Embalagens</b></Navbar.Brand>
             </Navbar>
@@ -86,12 +86,12 @@ function Login() {
                             </Col>
 
                         </Row>
-                        <Row >
+                        <Row>
                             <Col md={12} className={styles.buttonSalva}>
                                 <button type="submit" className="btn btn-primary"
                                         disabled={loading}>
                                     {loading ? (
-                                        <Spinner animation="border" size="sm" />
+                                        <Spinner animation="border" size="sm"/>
                                     ) : (
                                         "Entrar"
                                     )}
